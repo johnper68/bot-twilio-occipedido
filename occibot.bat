97faa159c73fb9b055e5a4d6a0de0512a1881c20
johnper68 <johnrper1968@gmail.com>
2025-07-16 16:00:38 -0500

    Delete occibot.bat

--- occibot.bat
+++
@@ -1,7 +0,0 @@
-echo "# bot-twilio-occipedido" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/johnper68/bot-twilio-occipedido.git
-git push -u origin main
